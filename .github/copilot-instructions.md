019d9ae541e41ae7bc3b091bc99b30488a38339b
Морон . <sisandr333@gmail.com>
2026-07-24 20:57:42 +0300

    feat: added player attack

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -1,4 +1,8 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: Forgotten Minute
 - Unity version: Unity 6000.5.4f1
+- Active game object:
+  - Name: TestEnemy
+  - Tag: Untagged
+  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,8 @@
-using Unity.VisualScripting.Antlr3.Runtime;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
-
-[RequireComponent(typeof(Rigidbody2D), typeof(Camera))]
+using UnityEngine.AI;
+[RequireComponent(typeof(Rigidbody2D), typeof(Camera), typeof(PolygonCollider2D))]
 public class Player : MonoBehaviour
 {
     private static Player _instance;
@@ -13,7 +14,7 @@ public class Player : MonoBehaviour
 
     [Header("Damage")]
     [SerializeField] private int _damage = 2;
-    [SerializeField] private int _attackCooldown = 1;
+    private bool _canAttack = true;
 
     [Header("Speed")]
     private float _currentSpeed = 0;
@@ -21,9 +22,11 @@ public class Player : MonoBehaviour
     [SerializeField] private float _sprintMultiplayer = 1.5f;
 
     private Vector2Int _currentRoomCoords;
+
     public Vector2Int CurrentRoomCoords => _currentRoomCoords;
 
     private Rigidbody2D _rb;
+    private PolygonCollider2D _attackCollider;
 
     private void Awake()
     {
@@ -32,9 +35,15 @@ public class Player : MonoBehaviour
         _instance = this;
 
         _rb = GetComponent<Rigidbody2D>();
+        _attackCollider = GetComponent<PolygonCollider2D>();
 
         _health = _maxHealth;
         _currentSpeed = _walkSpeed;
+    }
+
+    private void Start()
+    {
+        AttackColliderOff();
     }
 
     void Update()
@@ -44,15 +53,45 @@ public class Player : MonoBehaviour
 
     private void OnEnable()
     {
+        GameInput.Instance.Actions.Player.Attack.performed += Attack_performed;
+
         GameInput.Instance.Actions.Player.Sprint.started += Sprint_started;
         GameInput.Instance.Actions.Player.Sprint.canceled += Sprint_canceled;
     }
 
     private void OnDisable()
     {
+        GameInput.Instance.Actions.Player.Attack.performed -= Attack_performed;
+
         GameInput.Instance.Actions.Player.Sprint.started -= Sprint_started;
         GameInput.Instance.Actions.Player.Sprint.canceled -= Sprint_canceled;
     }
+
+    private void Attack_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        Debug.Log("Attack");
+        if (_canAttack)
+        {
+            _canAttack = false;
+            //_animator.SetTrigger(IS_ATTACK);
+            
+            AttackColliderOn();
+            StartCoroutine(AttackCooldown());
+        }
+    }
+
+    private IEnumerator AttackCooldown()
+    {
+        yield return new WaitForSeconds(Time.fixedDeltaTime);
+        AttackColliderOff();
+        yield return new WaitForSeconds(1.5f);
+        AttackEndTrigger();
+    }
+
+    public void AttackColliderOn() => _attackCollider.enabled = true;
+    public void AttackColliderOff() => _attackCollider.enabled = false;
+
+    public void AttackEndTrigger() => _canAttack = true;
 
     private void Sprint_started(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
@@ -64,7 +103,7 @@ public class Player : MonoBehaviour
         _currentSpeed = _walkSpeed;
     }
 
-    private void Move()
+    private void Move() 
     {
         var moveVector = GameInput.Instance.GetMoveVector();
 
@@ -74,4 +113,17 @@ public class Player : MonoBehaviour
     public void EnterRoom(Vector2Int roomCoords) => _currentRoomCoords = roomCoords;
 
     public void TakeDamage(int damage) => _health -= damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!_attackCollider.IsTouching(collision))
+            return;
+
+            Debug.Log("Attack detected something");
+        if (collision.TryGetComponent(out EnemyAI enemy))
+        {
+            enemy.TakeDamage(_damage);
+            Debug.Log("Attack performed");
+        }
+    }
 }
